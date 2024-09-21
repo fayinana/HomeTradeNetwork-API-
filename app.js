7d462918d401f66f59fdf0e3fda99a3467b5d3d9
fayinana <fekedeananiya@gmail.com>
2024-09-21 13:46:13 +0300

    add cors for socket.io

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-  origin: ["http://localhost:5173" , "https://home-trade-network-front-end.vercel.app"],
+  origin: [
+    "http://localhost:5173",
+    "https://home-trade-network-front-end.vercel.app",
+  ],
   credentials: true,
 };
 
@@ -37,9 +40,7 @@ const getUser = (userId) => {
 };
 
 const io = require("socket.io")(8900, {
-  cors: {
-    origin: "http://localhost:5173",
-  },
+  cors: corsOptions,
 });
 
 io.on("connection", (socket) => {
